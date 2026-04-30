1bc18dca43ff381a980a32a2b6ebc8e4738a17fd
GitHub Action <>
2026-04-30 23:11:15 +0000

    Release 2.11.4-beta.1

---
+++ sdk/src/main/java/com/deuna/maven/widgets/payment_widget/PaymentWidgetCallbacks.kt
@@ -0,0 +1,26 @@
+package com.deuna.maven.widgets.payment_widget
+
+import com.deuna.maven.widgets.checkout_widget.CheckoutEvent
+import com.deuna.maven.shared.Json
+import com.deuna.maven.shared.OnClosed
+import com.deuna.maven.shared.PaymentsError
+import com.deuna.maven.shared.VoidCallback
+
+typealias OnSuccess = (data: Json) -> Unit
+typealias OnError = (type: PaymentsError) -> Unit
+typealias OnCardBinDetected = (Json?) -> Unit
+typealias OnInstallmentSelected = (Json?) -> Unit
+typealias OnEventDispatch<S, E> = (E, S) -> Unit
+
+
+
+// Class defining the different callbacks that can be invoked by the payment widget
+class PaymentWidgetCallbacks {
+    var onSuccess: OnSuccess? = null
+    var onError: OnError? = null
+    var onClosed: OnClosed? = null
+    var onCardBinDetected: OnCardBinDetected? = null
+    var onInstallmentSelected: OnInstallmentSelected? = null
+    var onPaymentProcessing: VoidCallback? = null
+    var onEventDispatch: OnEventDispatch<Json, CheckoutEvent>? = null
+}
